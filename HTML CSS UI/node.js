97a964ef73067502c0fed76f7b5e63893fa89daf
sivalakshmi054 <lsivalakshmi054@gmail.com>
2022-01-18 12:58:01 +0530

    first commit

---
+++ HTML CSS UI/node.js
@@ -0,0 +1,13 @@
+
+
+function  somecode() {
+    for (var i = 0; i <=2005000; i+=1SS) {
+        var j=i;
+        
+        
+    } 
+    postMessage(i);
+        setTimeout("somecode()",500);
+    // document.getElementById('p1').innerHTML="Completed "  + j  + " Iterations";
+}
+somecode();
